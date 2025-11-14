db4e45901e5cb4e613181752530a1806390df7dd
AAQIB AHMED KHAN <aaqibahmed365@gmail.com>
2025-11-15 00:07:15 +0500

    Fix typo in stop function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe,use_container_width=True)
-st.stopt()
+st.stop()
 
 # Convert Snowpark DataFrame to simple Python list
 fruit_names = [row.FRUIT_NAME for row in my_dataframe.select("FRUIT_NAME").collect()]
