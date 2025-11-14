1efd415b21f788d69832d94aaa4090b1f84a8198
AAQIB AHMED KHAN <aaqibahmed365@gmail.com>
2025-11-15 00:04:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,9 @@ if smoothie_name:
 # Snowflake session and fruit options
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options")
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe,use_container_width=True)
+st.stopt()
 
 # Convert Snowpark DataFrame to simple Python list
 fruit_names = [row.FRUIT_NAME for row in my_dataframe.select("FRUIT_NAME").collect()]
